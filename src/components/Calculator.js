d7b32fa57ea39837c1b4d84986a624b534e4f8ab
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-01-04 15:38:08 -0600

    Convert to class and style

---
+++ src/components/Calculator.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Button from './Button';
+// import Row from './Row';
+
+export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const buttonNames = [
+      'AC', '+/-', '%', 'U00F7',
+      '7', '8', '9', 'x',
+      '4', '5', '6', '-',
+      '1', '2', '3', '+',
+      '0', '.', '=',
+    ];
+    const buttonsArray = [];
+    buttonNames.forEach((button) => buttonsArray.push(<Button name={button} />));
+
+    return (
+      <div id="calculator">
+        <div id="calc-display">0</div>
+        <div id="calc-buttons">
+          {buttonsArray}
+        </div>
+      </div>
+    );
+  }
+}
