ce3f4a25ef0c2fe613bab20d57ef5a94e2042576
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-01-06 09:34:18 -0600

    Change Button from class to func

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,31 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  clickHandler = (e) => {
-    const { click } = this.props;
+const Button = (props) => {
+  const clickHandler = (e) => {
+    const { click } = props;
     click(e.target.value);
-  }
+  };
 
-  render = () => {
-    const { name } = this.props;
-    return (
-      <input
-        type="button"
-        id={name}
-        value={name}
-        key={name}
-        onClick={this.clickHandler}
-      />
-    );
-  }
-}
+  const { name } = props;
+  return (
+    <input
+      type="button"
+      id={name}
+      value={name}
+      key={name}
+      onClick={clickHandler}
+    />
+  );
+};
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   click: PropTypes.func.isRequired,
 };
+export default Button;
