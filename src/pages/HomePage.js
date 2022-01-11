084e741a4ec9b384ab5331ed27f5b9073bd0092f
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-01-11 14:02:41 -0600

    Add Styling HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,19 +4,47 @@ import Header from '../components/Header';
 const HomePage = () => (
   <section className="Home">
     <Header />
-    <h2>
-      Welcome to our page!
-    </h2>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      Natus mollitia optio reprehenderit temporibus voluptatum recusandae fugiat non.
-      Qui, temporibus! Animi fugiat nostrum vitae dolore nisi laborum ut culpa explicabo quisquam?
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      Natus mollitia optio reprehenderit temporibus voluptatum recusandae fugiat non.
-      Qui, temporibus! Animi fugiat nostrum vitae dolore nisi laborum ut culpa explicabo quisquam?
-    </p>
+    <main>
+      <h2>
+        Welcome to our page!
+      </h2>
+      <p>
+        Ut congue convallis massa eget rhoncus.
+        Vivamus interdum semper lacus, at viverra ante porttitor in.
+        Nullam mattis egestas justo, tempus efficitur massa luctus a.
+        Praesent mollis vehicula vehicula. Aliquam in justo faucibus,
+        efficitur neque nec, convallis tortor. Sed dapibus turpis ac
+        massa commodo egestas. Praesent id eros urna. Praesent sed
+        auctor dolor. Morbi condimentum euismod augue nec varius.
+        Mauris mollis tincidunt massa. Mauris et orci quis nisl porta finibus.
+        Etiam accumsan vulputate lobortis. Mauris non nunc ultrices
+        metus vulputate iaculis. Vivamus ultrices arcu et nulla ultricies,
+        sit amet luctus dui mattis. Nunc blandit tincidunt congue
+      </p>
+
+      <p>
+        Sed ac metus fermentum nisi lacinia sodales. Nullam porta
+        tristique justo et laoreet. Vestibulum pulvinar, purus a finibus
+        commodo, quam lectus imperdiet arcu, vel sollicitudin augue ligula
+        quis lorem. Nunc vehicula lectus nec quam interdum, vitae mattis
+        sapien iaculis. Duis et dui sed lorem hendrerit sollicitudin.
+        Donec ut justo tincidunt, finibus nisi ut, fringilla augue. Donec
+        a nibh nec libero pellentesque semper et vel lacus. Integer quis
+        magna quam
+      </p>
+
+      <p>
+        Mauris tincidunt laoreet nisi, tincidunt tincidunt est dictum vitae.
+        Duis cursus imperdiet mauris in tincidunt. Etiam porta fermentum dignissim.
+        Integer venenatis cursus neque eget vulputate. Vestibulum at molestie risus,
+        non cursus lacus. Maecenas vulputate felis risus, vel placerat justo vehicula
+        sit amet. Ut tincidunt semper lacus, sed aliquet nisl ornare quis.
+        Proin hendrerit quam eget tellus tincidunt blandit. Mauris mollis diam vitae
+        lobortis consectetur. Sed eu iaculis dui, at varius metus. Maecenas semper
+        tortor eget diam rhoncus, non rutrum justo cursus. Nullam vitae pellentesque
+        urna, ut suscipit elit. Nulla porta eros justo, sit amet ullamcorper sem scelerisque quis.
+      </p>
+    </main>
   </section>
 );
 export default HomePage;
