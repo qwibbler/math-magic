5caf4c5e111be5054907f2d7645c8aac8418c26e
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-01-12 14:50:27 -0600

    Add nomatch test

---
+++ src/pages/NoMatch.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NoMatch from './NoMatch';
+import renderer from 'react-test-renderer';
+import { HashRouter as Router } from 'react-router-dom';
+
+test('NoMatch', () => {
+  const tree = renderer
+    .create(
+      <Router>
+        <NoMatch />
+      </Router>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
