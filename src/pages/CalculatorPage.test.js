1e62c434a8eafd519953eb8e9ccb92218495e0c5
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-01-12 14:45:53 -0600

    Add Calc Page test

---
+++ src/pages/CalculatorPage.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CalculatorPage from './CalculatorPage';
+import renderer from 'react-test-renderer';
+import { HashRouter as Router } from 'react-router-dom';
+
+describe('Header', () => {
+  test('Header', () => {
+    const tree = renderer.create(
+      <Router>
+        <CalculatorPage />
+      </Router>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
