3289114cebd5c3fd64b069c7e899137e55dd358a
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-01-12 15:14:30 -0600

    Add link tests to header & Update readme

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react';
 import { HashRouter as Router } from 'react-router-dom';
 
 describe('Header', () => {
@@ -13,4 +14,33 @@ describe('Header', () => {
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  describe('Links', () => {
+    test('Home', () => {
+      render(
+        <Router>
+          <Header />
+        </Router>
+      );
+      expect(document.querySelectorAll("a")[0].getAttribute("href")).toBe('#/')
+    })
+
+    test('Calculator', () => {
+      render(
+        <Router>
+          <Header />
+        </Router>
+      );
+      expect(document.querySelectorAll("a")[1].getAttribute("href")).toBe('#/calculator')
+    })
+
+    test('Quote', () => {
+      render(
+        <Router>
+          <Header />
+        </Router>
+      );
+      expect(document.querySelectorAll("a")[2].getAttribute("href")).toBe('#/quote')
+    })
+  })
 });
