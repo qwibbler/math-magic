41c5ea84acd9262bf6432be6791decfd57c4c121
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-01-12 11:34:46 -0600

    Finish calculate tests, mark where calculate logic can be improved

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,24 +1,17 @@
 import calculate from './calculate';
 
 describe('Test: Calculate:', () => {
-  const emptyObj = {
-    total: null,
-    next: null,
-    operation: null,
-  }
-  const nonEmptyObj = {
-    total: '1',
-    next: '0',
-    operation: '*',
-  }
-
   test('Button = AC', () => {
     expect(calculate({
       total: '1',
       next: '0',
       operation: '*',
-    }, 'AC')).toEqual(emptyObj)
-  })
+    }, 'AC')).toEqual({
+      total: null,
+      next: null,
+      operation: null,
+    });
+  });
 
   describe('Button === number', () => {
     test('Obj next = 0, Button = 0', () => {
@@ -26,8 +19,8 @@ describe('Test: Calculate:', () => {
         total: '1',
         next: '0',
         operation: '*',
-      }, '0')).toEqual({})
-    })
+      }, '0')).toEqual({});
+    });
 
     test('Obj operation, next = 2, Button = 3', () => {
       expect(calculate({
@@ -38,8 +31,8 @@ describe('Test: Calculate:', () => {
         total: '1',
         next: '23',
         operation: '*',
-      })
-    })
+      });
+    });
 
     test('Obj operation, next = null, Button = 3', () => {
       expect(calculate({
@@ -50,8 +43,8 @@ describe('Test: Calculate:', () => {
         total: '1',
         next: '3',
         operation: '*',
-      })
-    })
+      });
+    });
 
     test('Obj operation = null, next = 2, Button = 3', () => {
       expect(calculate({
@@ -61,8 +54,8 @@ describe('Test: Calculate:', () => {
       }, '3')).toEqual({
         total: null,
         next: '23',
-      })
-    })
+      });
+    });
 
     test('Obj operation = null, next = null, Button = 3', () => {
       expect(calculate({
@@ -72,9 +65,9 @@ describe('Test: Calculate:', () => {
       }, '3')).toEqual({
         total: null,
         next: '3',
-      })
-    })
-  })
+      });
+    });
+  });
 
   describe('Button === .', () => {
     test('Obj next = 1.2, Button = .', () => {
@@ -86,8 +79,8 @@ describe('Test: Calculate:', () => {
         total: '1',
         next: '1.2',
         operation: null,
-      })
-    })
+      });
+    });
 
     test('Obj next = 12, Button = .', () => {
       expect(calculate({
@@ -98,8 +91,8 @@ describe('Test: Calculate:', () => {
         total: '1',
         next: '12.',
         operation: null,
-      })
-    })
+      });
+    });
 
     test('Obj operation, next = null, Button = .', () => {
       expect(calculate({
@@ -107,8 +100,214 @@ describe('Test: Calculate:', () => {
         next: null,
         operation: '+',
       }, '.')).toEqual({
-        next: '0.'
-      })
-    })
-  })
-})
+        next: '0.',
+      });
+    });
+
+    test('Obj operation and next = null, total = 1.2, Button = .', () => {
+      expect(calculate({
+        total: '1.2',
+        next: null,
+        operation: null,
+      }, '.')).toEqual({});
+    });
+
+    test('Obj operation and next = null, total = 12, Button = .', () => {
+      expect(calculate({
+        total: '12',
+        next: null,
+        operation: null,
+      }, '.')).toEqual({
+        total: '12.',
+      });
+    });
+
+    test('Obj = null, Button = .', () => {
+      expect(calculate({
+        total: null,
+        next: null,
+        operation: null,
+      }, '.')).toEqual({
+        total: '0.',
+      });
+    });
+  });
+
+  describe('Button === =', () => {
+    test('Obj operation, total = 1, next = 2, Button = =', () => {
+      expect(calculate({
+        total: '1',
+        next: '2',
+        operation: '+',
+      }, '=')).toEqual({
+        total: '3',
+        next: null,
+        operation: null,
+      });
+    });
+
+    test('Obj operation = null, next = 2, total = 1, Button = =', () => {
+      expect(calculate({
+        total: '1',
+        next: '2',
+        operation: null,
+      }, '=')).toEqual({});
+    });
+
+    test('Obj operation, next = null, total = 1, Button = =', () => {
+      expect(calculate({
+        total: '1',
+        next: null,
+        operation: '+',
+      }, '=')).toEqual({});
+    });
+
+    test('Obj operation = null, next = null, total = 1, Button = =', () => {
+      expect(calculate({
+        total: '1',
+        next: null,
+        operation: null,
+      }, '=')).toEqual({});
+    });
+  });
+
+  describe('Button === +/-', () => {
+    test('Obj next = 2, Button = +/-', () => {
+      expect(calculate({
+        total: '1',
+        next: '2',
+        operation: '+',
+      }, '+/-')).toEqual({
+        total: '1',
+        next: '-2',
+        operation: '+',
+      });
+    });
+
+    test('Obj next = -2, Button = +/-', () => {
+      expect(calculate({
+        total: '1',
+        next: '-2',
+        operation: '+',
+      }, '+/-')).toEqual({
+        total: '1',
+        next: '2',
+        operation: '+',
+      });
+    });
+
+    test('Obj next = null, total = 1, Button = +/-', () => {
+      expect(calculate({
+        total: '1',
+        next: null,
+        operation: '+',
+      }, '+/-')).toEqual({
+        total: '-1',
+        next: null,
+        operation: '+',
+      });
+    });
+
+    test('Obj next = null, total = -1, Button = +/-', () => {
+      expect(calculate({
+        total: '-1',
+        next: null,
+        operation: '+',
+      }, '+/-')).toEqual({
+        total: '1',
+        next: null,
+        operation: '+',
+      });
+    });
+
+    test('Obj next = null, total = null, Button = +/-', () => {
+      expect(calculate({
+        total: null,
+        next: null,
+        operation: '+',
+      }, '+/-')).toEqual({});
+    });
+  });
+
+  test('Obj operation and next = null, total = 1, Button = +', () => {
+    expect(calculate({
+      total: '1',
+      next: null,
+      operation: null,
+    }, '+')).toEqual({
+      total: '1',
+      next: null,
+      operation: '+',
+    });
+  });
+
+  test('Obj operation, next = null, total = 1, Button = +', () => {
+    expect(calculate({
+      total: '1',
+      next: null,
+      operation: '-',
+    }, '+')).toEqual({
+      total: '1',
+      next: null,
+      operation: '+',
+    });
+  });
+
+  test('Obj operation, next = 2, total = 1, Button = -', () => {
+    expect(calculate({
+      total: '1',
+      next: '2',
+      operation: '+',
+    }, '-')).toEqual({
+      total: '3',
+      next: null,
+      operation: '-',
+    });
+  });
+
+  test('Obj operation, next = 2, total = 1, Button = -', () => {
+    expect(calculate({
+      total: '1',
+      next: '2',
+      operation: '+',
+    }, '-')).toEqual({
+      total: '3',
+      next: null,
+      operation: '-',
+    });
+  });
+
+  test('Obj operation and next = null, total = 1, Button = -', () => {
+    expect(calculate({
+      total: '1',
+      next: null,
+      operation: null,
+    }, '-')).toEqual({
+      total: '1',
+      next: null,
+      operation: '-',
+    });
+  });
+
+  test('Obj operation and next and total = null, Button = -', () => {
+    expect(calculate({
+      total: null,
+      next: null,
+      operation: null,
+    }, '-')).toEqual({
+      operation: '-',
+    });
+  });
+
+  test('Obj operation and total = null, next = 1, Button = -', () => {
+    expect(calculate({
+      total: null,
+      next: '1',
+      operation: null,
+    }, '-')).toEqual({
+      total: '1',
+      next: null,
+      operation: '-',
+    });
+  });
+});
--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -2,34 +2,34 @@ import operate from './operate';
 
 describe('Test: Operate:', () => {
   test('1 + 2 = 3', () => {
-    expect(operate('1', '2', '+')).toBe('3')
-  })
+    expect(operate('1', '2', '+')).toBe('3');
+  });
 
   test('3 - 2 = 1', () => {
-    expect(operate('3', '2', '-')).toBe('1')
-  })
+    expect(operate('3', '2', '-')).toBe('1');
+  });
 
   test('2 x 3 = 6', () => {
-    expect(operate('2', '3', 'x')).toBe('6')
-  })
+    expect(operate('2', '3', 'x')).toBe('6');
+  });
 
   test('6 ÷ 2 = 3', () => {
-    expect(operate('6', '2', '÷')).toBe('3')
-  })
+    expect(operate('6', '2', '÷')).toBe('3');
+  });
 
   test('6 ÷ 0 = error', () => {
-    expect(operate('6', '0', '÷')).toBe("Can't divide by 0.")
-  })
+    expect(operate('6', '0', '÷')).toBe("Can't divide by 0.");
+  });
 
   test('6 % 5 = 1', () => {
-    expect(operate('6', '5', '%')).toBe('1')
-  })
+    expect(operate('6', '5', '%')).toBe('1');
+  });
 
   test('6 % 0 = error', () => {
-    expect(operate('6', '0', '%')).toBe("Can't find modulo as can't divide by 0.")
-  })
+    expect(operate('6', '0', '%')).toBe("Can't find modulo as can't divide by 0.");
+  });
 
   test('6 ^ 0 = error', () => {
-    expect(() => { operate('6', '0', '^'); }).toThrowError("Unknown operation '^'")
-  })
-})
+    expect(() => { operate('6', '0', '^'); }).toThrowError("Unknown operation '^'");
+  });
+});
