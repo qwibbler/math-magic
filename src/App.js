886fb171c05d5c9b300288680435e3df70e60047
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-01-11 18:44:01 -0600

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import CalculatorPage from './pages/CalculatorPage';
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import CalculatorPage from './pages/CalculatorPage';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
